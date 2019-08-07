dab84ac3164bdc28c8d2250cffe62c2938d203fe
swayne275 <swayne275@gmail.com>
2019-08-06 18:38:04 -0600

    initial commit creating beta structure

---
+++ gcalwrapper/gcalwrapper.go
@@ -0,0 +1,12 @@
+// Convenience wrapper for Google Calendar API V3
+
+/*
+TODO
+This will start off as part of the showCal project, but should probably
+be spun into its own package down the road. It will also initially
+include the OAuth2 and GCal bits in this one file, but those will
+likely be split into two files (in the same package) when I'm closer
+to completing this component
+*/
+
+package gcalwrapper
