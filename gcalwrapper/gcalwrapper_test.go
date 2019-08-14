e9aef5e915b4639783945575efd418ea792836a1
swayne275 <swayne275@gmail.com>
2019-08-14 13:13:04 -0600

    try changing import style

--- gcalwrapper/gcalwrapper_test.go
+++ gcalwrapper/gcalwrapper_test.go
@@ -1,9 +1,10 @@
 package gcalwrapper
 
 import (
-	"showcal-backend-go/tvshowdata"
 	"testing"
 	"time"
+
+	"github.com/swayne275/showcal-backend-go/tvshowdata"
 )
 
 func TestFormatEpisodeForCalendar(t *testing.T) {
